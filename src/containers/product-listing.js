5cb8bf56f445f81eb2a0174e04f70ac81fc9314b
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-09 17:27:01 +0300

    axios installed, useEffect got data from fake API

--- src/containers/product-listing.js
+++ src/containers/product-listing.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useSelector } from 'react-redux';
 import ProductComponent from './product-component';
 
 const ProductListing = () => {
     
     const products = useSelector(state => state);
+
+    const fetchProducts = async() => {
+        const resp = await axios
+        .get('https://fakestoreapi.com/products')
+        .catch((err)=> {
+            console.log('an error',err);
+        })
+        console.log(resp);
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    },[])
 
     console.log(products);
 
