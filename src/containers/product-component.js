22e4743020aa64b71e420a5b5709106355becf41
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-11 18:34:38 +0300

    renders on browser

--- src/containers/product-component.js
+++ src/containers/product-component.js
@@ -6,21 +6,34 @@ const ProductComponent = () => {
     const products = useSelector(state => state.allProducts.products);
 
     // const {id, title} = products[0];
+    const renderProducts = products.map((product) => {
+
+        const {id,title, image, price, description, category } = product;
+
+        return (
+            <div className='four wide column' key={id}>
+                <div className='ui link cards'>
+                    <div className='card'>
+
+                        <div className='image'>
+                            <img src={image} alt={title}/>
+                        </div>
+                        <div className='content'>
+                            <div className='header'>{title}</div>
+                            <div className='meta price'>$ {price}</div>
+                            <div className='meta'>{category}</div>
+                        </div>
+
+                    </div>
+                </div>        
+            </div>
+        )
+    })
 
     return (
-    <div className='four column wide'>
-        <div className='ui link cards'>
-            <div className='card'>
+        <>{renderProducts}</>
+    )
 
-                <div className='image'></div>
-                <div className='content'>
-                    {/* <div className='header'>{title}</div> */}
-                </div>
-
-            </div>
-        </div>        
-    </div>
-    );
 }
 
 export default ProductComponent;
