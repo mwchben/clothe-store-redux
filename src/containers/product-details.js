842173bc42d8e79a1e9f2e3d6a79881e59c90966
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-05-14 18:44:46 +0300

    Displays selected product

--- src/containers/product-details.js
+++ src/containers/product-details.js
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from 'react-redux';
 const ProductDetails = () => {
 
     const product = useSelector(state => state.product);
+    const {image, title, price, category, description} = product;
+
     const { productId } = useParams();
     const dispatch = useDispatch();
     console.log(product);
@@ -33,10 +35,41 @@ const ProductDetails = () => {
 
 
     return (
-    <div >
-        <br></br>
-        <h4>ProductDetails</h4>
+    <div className='ui grid container'>
+
+        {Object.keys(product).length === 0 ? (
+            <div>Product Loading...</div>
+        ) : (
+            <div className='ui placeholder segment'>
+                <div className='ui two column stackable center aligned grid'>
+                    <div className='ui vertical divider'>AND</div>
+                    <div className='middle aligned row'>
+                        <div className='column 1p'>
+                            <img className='ui fluid image' src={image} />  
+                        </div>
+                        <div className='column rp'>
+                            <h1>{title}</h1> 
+                            <h2>
+                                <a className='ui teal tag label'>${price}</a>
+                            </h2> 
+                            <h2 className='ui brown block header'>{category}</h2> 
+                            <p>{description}</p>
+                            <div className='ui vertical animated button' tabIndex="0">
+                                <div className='hidden content'>
+                                    <i className='shop icon'></i>
+                                </div>  
+                                <div className='visible content'>Add to cart</div>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
     </div>
+
     );
 }
 
